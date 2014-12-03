3ee9521ab6ab4b39f0df7502e942d03a045f3a5b
aldente39 <>
2014-12-03 14:12:08 +0900

    fixed bicg.c

--- src/bicg.c
+++ src/bicg.c
@@ -4,15 +4,14 @@
 
 int dbicg (dspmat *mat, double *b,
             double tol, int max_iter, double *x) {
+    ////////// Initialization //////////
     int i;
     double error = 0;
-    double alpha, beta, fnb;
-    double *r, *r_old, *sr, *sr_old, *p, *sp, *ap, *atp;
+    double alpha, beta, norm_b, r_dot, r_dot_old;
+    double *r, *sr, *p, *sp, *ap, *atp;
     MKL_INT n = *mat->row_size;
     r = (double *) malloc(n * sizeof(double));
-    r_old = (double *) malloc(n * sizeof(double));
     sr = (double *) malloc(n * sizeof(double));
-    sr_old = (double *) malloc(n * sizeof(double));
     p = (double *) malloc(n * sizeof(double));
     sp = (double *) malloc(n * sizeof(double));
     ap = (double *) malloc(n * sizeof(double));
@@ -27,32 +26,51 @@ int dbicg (dspmat *mat, double *b,
     cblas_dcopy(n, tmp, 1, sr, 1);
     cblas_dcopy(n, r, 1, p, 1);
     cblas_dcopy(n, sr, 1, sp, 1);
-    fnb = cblas_dnrm2(n, b, 1);
+    r_dot_old = cblas_ddot(n, r, 1, sr, 1);
+    norm_b = cblas_dnrm2(n, b, 1);
 
+    ////////// Iteration //////////
     for (i = 0; i < max_iter; i++) {
-        cblas_dcopy(n, r, 1, r_old, 1);
-        cblas_dcopy(n, sr, 1, sr_old, 1);
+        // Compute A * p.
         mkl_cspblas_dcsrgemv("n", mat->row_size, mat->value,
                      mat->I, mat->J, p, ap);
+
+        // Compute A^{T} * sp.
         mkl_cspblas_dcsrgemv("t", mat->row_size, mat->value,
                      mat->I, mat->J, sp, atp);
-        alpha = cblas_ddot(n, r, 1, sr, 1) / cblas_ddot(n, ap, 1, sp, 1);
+
+        // Compute alpha.
+        alpha = r_dot_old / cblas_ddot(n, ap, 1, sp, 1);
+
+        // Compute the approximate solution x.
         cblas_daxpy(n, alpha, p, 1, x, 1);
+
+        // Compute the residual r.
         cblas_daxpy(n, -alpha, ap, 1, r, 1);
+
+        // Compute the shadow residual sr.
         cblas_daxpy(n, -alpha, atp, 1, sr, 1);
-        beta = cblas_ddot(n, r, 1, sr, 1)
-               / cblas_ddot(n, r_old, 1, sr_old, 1);
+
+        // Compute beta.
+        r_dot = cblas_ddot(n, r, 1, sr, 1);
+        beta = r_dot / r_dot_old;
+        r_dot_old = r_dot;
+
+        // Compute p.
         cblas_dscal(n, beta, p, 1);
         cblas_daxpy(n, 1, r, 1, p, 1);
+
+        // Compute shadow p.
         cblas_dscal(n, beta, sp, 1);
         cblas_daxpy(n, 1, sr, 1, sp, 1);
 
-        error = cblas_dnrm2(n, r, 1) / fnb;
+        error = cblas_dnrm2(n, r, 1) / norm_b;
         if (error < tol) {
             break;
         }
     }
 
+    ////////// Finalization //////////
     printf("%d, %e\n",i,error);
     free(tmp);
     free(atp);
@@ -60,8 +78,7 @@ int dbicg (dspmat *mat, double *b,
     free(sp);
     free(p);
     free(sr);
-    free(r_old);
-    free(sr_old);
+    free(r);
     
     if (error >= tol) {
         return -1;
