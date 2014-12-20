903b8240025fd2965f7d5382cfb02e6aa94a8bed
aldente39 <>
2014-12-21 03:08:00 +0900

    fixed return value and allocation.

--- src/bicgstab.c
+++ src/bicgstab.c
@@ -4,20 +4,21 @@
 
 int dbicgstab(dspmat *mat, double *b, double tol,
                                        int max_iter, double *x) {
+    ////////// Initialization //////////
     int i;
     double error = 0;
-    double alpha, beta, omega, fnb;
-    double *r, *r_old, *rs, *s, *p, *ap, *as;
+    double alpha, beta, omega, rho, rho_new, norm_b;
+    double *r, *rs, *s, *p, *ap, *as, *tmp, *base;
     MKL_INT n = *mat->row_size;
-    r = (double *) malloc(n * sizeof(double));
-    r_old = (double *) malloc(n * sizeof(double));
-    rs = (double *) malloc(n * sizeof(double));
-    s = (double *) malloc(n * sizeof(double));
-    p = (double *) malloc(n * sizeof(double));
-    ap = (double *) malloc(n * sizeof(double));
-    as = (double *) malloc(n * sizeof(double));
+    base = (double *)malloc(n * 7 * sizeof(double));
+    r = base;
+    rs = &base[n];
+    s = &base[n * 2];
+    p = &base[n * 3];
+    ap = &base[n * 4];
+    as = &base[n * 5];
+    tmp = &base[n * 6];
 
-    double *tmp = (double *) malloc(n * sizeof(double));
     mkl_cspblas_dcsrgemv("n", mat->row_size, mat->value,
                    mat->I, mat->J, x, tmp);
     cblas_dscal(n, -1, tmp, 1);
@@ -25,13 +26,14 @@ int dbicgstab(dspmat *mat, double *b, double tol,
     cblas_dcopy(n, tmp, 1, r, 1);
     cblas_dcopy(n, r, 1, p, 1);
     cblas_dcopy(n, r, 1, rs, 1);
-    fnb = cblas_dnrm2(n, b, 1);
+    norm_b = cblas_dnrm2(n, b, 1);
+    rho = cblas_ddot(n, rs, 1, r, 1);
 
+    ////////// Iteration //////////
     for (i = 0; i < max_iter; i++) {
-        cblas_dcopy(n, r, 1, r_old, 1);
         mkl_cspblas_dcsrgemv("n", mat->row_size, mat->value,
                      mat->I, mat->J, p, ap);
-        alpha = cblas_ddot(n, rs, 1, r, 1) / cblas_ddot(n, rs, 1, ap, 1);
+        alpha = rho / cblas_ddot(n, rs, 1, ap, 1);
 
         cblas_dcopy(n, r, 1, s, 1);
         cblas_daxpy(n, -alpha, ap, 1, s, 1);
@@ -46,30 +48,26 @@ int dbicgstab(dspmat *mat, double *b, double tol,
         cblas_dcopy(n, s, 1, r, 1);
         cblas_daxpy(n, -omega, as, 1, r, 1);
 
-        beta = (alpha / omega) *
-                cblas_ddot(n, rs, 1, r, 1) / cblas_ddot(n, rs, 1, r_old, 1);
+        rho_new = cblas_ddot(n, rs, 1, r, 1);
+        beta = alpha / omega * rho_new / rho;
+        rho = rho_new;
 
         cblas_daxpy(n, -omega, ap, 1, p, 1);
         cblas_dscal(n, beta, p, 1);
         cblas_daxpy(n, 1, r, 1, p, 1);
 
-        error = cblas_dnrm2(n, r, 1) / fnb;
+        error = cblas_dnrm2(n, r, 1) / norm_b;
         if (error < tol) {
             break;
         }
     }
 
-    free(tmp);
-    free(as);
-    free(ap);
-    free(p);
-    free(s);
-    free(r_old);
-    free(rs);
+    ////////// Finalization //////////
+    free(base);
     
     if (error >= tol) {
-        return -1;
+        return NOT_CONVERGED;
     }
-    return 0;
+    return i;
 }
 
