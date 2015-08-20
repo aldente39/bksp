7df7c766d1f08ac1dc8389b8356f92aa9432e766
aldente39 <>
2015-08-20 12:26:25 +0900

    added MATLAB interfaces for complex version of the bicg method and the bicgstabl method.

--- matlab/bicg.c
+++ matlab/bicg.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
-#include "bksp.h"
+#include <complex.h>
+#include <bksp.h>
 #include <mex.h>
 #include "bksp_matlab.h"
 
 void mexFunction(int nlhs, mxArray *plhs[], int nrhs,
                       const mxArray *prhs[]) {
 
-    ///// check input arguments.
+    // check input arguments.
     if (nrhs != 4) {
         mexErrMsgIdAndTxt("BKSP:bicg",
                           "Four inputs required.");
@@ -16,32 +17,78 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs,
 
     check_tolerance(prhs[2]);
     check_max_iteration(prhs[3]);
-    
-    ///// Initialization
+
     mwIndex *row, *col;
-    int ell, max_iter, i;
-    double *b, *x, tol;
-    dspmat *mat;
-    mat = (dspmat *)malloc(sizeof(dspmat));
+    int ell, max_iter, i, code;
+    double tol;
 
-    MATLABMatrix_to_CSRMatrix(prhs[0], mat);
+    if (mxGetPi(prhs[0]) == NULL) {
+        ////////// Initialization //////////
+        dspmat *mat;
+        double *b, *x;
 
-    *plhs = mxCreateDoubleMatrix(dspmat_rowsize(mat), 1, mxREAL);
-    x = mxGetPr(*plhs);
+        // convert MATLAB sparse matrix to CSR matrix.
+        mat = (dspmat *)malloc(sizeof(dspmat));
+        MATLABMatrix2dCSRMatrix(prhs[0], mat);
 
-    b = mxGetPr(prhs[1]);
-    tol = *mxGetPr(prhs[2]);
-    max_iter = (int)*mxGetPr(prhs[3]);
+        *plhs = mxCreateDoubleMatrix(dspmat_rowsize(mat), 1, mxREAL);
+        x = mxGetPr(*plhs);
+        b = mxGetPr(prhs[1]);
+        tol = *mxGetPr(prhs[2]);
+        max_iter = (int)*mxGetPr(prhs[3]);
 
-    /* for (i = 0; i < *mat->nnz; i++) {
-     *     mat->col[i] = (int)col[i];
-     * }
-     * for (i = 0; i <= dspmat_rowsize(mat); i++) {
-     *     mat->row[i] = (int)row[i];
-     * } */
+        ////////// execute the BiCG method. //////////
+        code = dbicg(mat, b, tol, max_iter, x);
 
-    // execute the BiCG method.
-    int code = dbicg(mat, b, tol, max_iter, x);
+        ////////// Finalization //////////
+        free(mat);
+    }
+    else if (mxIsComplex(prhs[0])) {
+        ////////// Initialization //////////
+        zspmat *mat;
+        double _Complex *b, *x;
+        double *b_re, *b_im, *x_re, *x_im;
+
+        // convert MATLAB sparse matrix to CSR matrix.        
+        mat = (zspmat *)malloc(sizeof(zspmat));
+        MATLABMatrix2zCSRMatrix(prhs[0], mat);
+
+        int size = zspmat_rowsize(mat);
+        b = (double _Complex *)malloc(sizeof(double _Complex) * size);
+        x = (double _Complex *)malloc(sizeof(double _Complex) * size);
+
+        *plhs = mxCreateDoubleMatrix(size, 1, mxCOMPLEX);
+        x_re = mxGetPr(*plhs);
+        x_im = mxGetPi(*plhs);
+        b_re = mxGetPr(prhs[1]);
+        b_im = mxGetPi(prhs[1]);
+        tol = *mxGetPr(prhs[2]);
+        max_iter = (int)*mxGetPr(prhs[3]);
+
+        if (b_im == NULL) {
+            b_im = (double *)calloc(size, sizeof(double));
+        }
+        for(i = 0; i < size; i++) {
+            b[i] = b_re[i] + b_im[i] * _Complex_I;
+            x[i] = x_re[i] + x_im[i] * _Complex_I;
+        }
+
+        ////////// execute the BiCG method. //////////
+        code = zbicg(mat, b, tol, max_iter, x);
+
+        ////////// Finalization //////////
+        // store approximate solution for MATLAB.
+        for (i = 0; i < size; i++) {
+            x_re[i] = creal(x[i]);
+            x_im[i] = cimag(x[i]);
+        }
+        mxSetPr(*plhs, x_re);
+        mxSetPi(*plhs, x_im);
+
+        free(mat);
+        free(b);
+        free(x);
+    }
 
     if (code < 0) {
         printf("The BiCG Method did not converged.\n");
@@ -49,9 +96,5 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs,
     else {
         printf("The BiCG Method converged at iteration %d.\n", code);
     }
-    printf("The true relative residual 2-norm : %e\n",
-           d2norm_true_res_u(mat, b, x));
-
-    free(mat);
 }
 
--- matlab/bicgstabl.c
+++ matlab/bicgstabl.c
@@ -1,44 +1,100 @@
 #include <stdio.h>
-#include "bksp.h"
+#include <complex.h>
+#include <bksp.h>
 #include <mex.h>
 #include "bksp_matlab.h"
 
 void mexFunction(int nlhs, mxArray *plhs[], int nrhs,
                       const mxArray *prhs[]) {
 
-    ///// check input arguments.
+    // check input arguments.
     if (nrhs != 5) {
-        mexErrMsgIdAndTxt("MyProg:mysqrt",
+        mexErrMsgIdAndTxt("BKSP:bicgstabl",
                           "Five inputs required.");
     }
 
     check_coefficient_and_rhs(prhs[0], prhs[1], "single");
 
     if (mxGetM(prhs[2]) != 1 || mxGetN(prhs[2]) != 1) {
-        mexErrMsgIdAndTxt("BKSP:blbicgstabl",
+        mexErrMsgIdAndTxt("BKSP:bicgstabl",
                           "Input parameter ell must be an integer.");
     }
 
     check_tolerance(prhs[3]);
     check_max_iteration(prhs[4]);
 
-    ///// Initialization
-    int ell, max_iter, size;
-    double *b, *x, tol;
-    dspmat *mat;
-    mat = (dspmat *)malloc(sizeof(dspmat));
+    ////////// Initialization //////////
+    mwIndex *row, *col;
+    int ell, max_iter, i, code;
+    double tol;
 
-    MATLABMatrix_to_CSRMatrix(prhs[0], mat);
-
-    *plhs = mxCreateDoubleMatrix(dspmat_rowsize(mat), 1, mxREAL);
-    x = mxGetPr(*plhs);
-    b = mxGetPr(prhs[1]);
     ell = *mxGetPr(prhs[2]);
     tol = *mxGetPr(prhs[3]);
     max_iter = (int)*mxGetPr(prhs[4]);
-    
-    // execute the BiCGSTAB(ell) method.
-    int code = dbicgstabl(mat, b, ell, tol, max_iter, x);
+
+    if (mxGetPi(prhs[0]) == NULL) {
+        ////////// Initialization //////////
+        dspmat *mat;
+        double *b, *x;
+
+        // convert MATLAB sparse matrix to CSR matrix.
+        mat = (dspmat *)malloc(sizeof(dspmat));
+        MATLABMatrix2dCSRMatrix(prhs[0], mat);
+
+        *plhs = mxCreateDoubleMatrix(dspmat_rowsize(mat), 1, mxREAL);
+        x = mxGetPr(*plhs);
+        b = mxGetPr(prhs[1]);
+
+        ////////// execute the BiCGSTAB(l) method. //////////
+        code = dbicgstabl(mat, b, ell, tol, max_iter, x);
+
+        ////////// Finalization //////////
+        free(mat);
+    }
+    else if (mxIsComplex(prhs[0])) {
+        ////////// Initialization //////////
+        zspmat *mat;
+        double _Complex *b, *x;
+        double *b_re, *b_im, *x_re, *x_im;
+
+        // convert MATLAB sparse matrix to CSR matrix.        
+        mat = (zspmat *)malloc(sizeof(zspmat));
+        MATLABMatrix2zCSRMatrix(prhs[0], mat);
+
+        int size = zspmat_rowsize(mat);
+        b = (double _Complex *)malloc(sizeof(double _Complex) * size);
+        x = (double _Complex *)malloc(sizeof(double _Complex) * size);
+
+        *plhs = mxCreateDoubleMatrix(size, 1, mxCOMPLEX);
+        x_re = mxGetPr(*plhs);
+        x_im = mxGetPi(*plhs);
+        b_re = mxGetPr(prhs[1]);
+        b_im = mxGetPi(prhs[1]);
+
+        if (b_im == NULL) {
+            b_im = (double *)calloc(size, sizeof(double));
+        }
+        for(i = 0; i < size; i++) {
+            b[i] = b_re[i] + b_im[i] * _Complex_I;
+            x[i] = x_re[i] + x_im[i] * _Complex_I;
+        }
+
+        ////////// execute the BiCGSTAB(l) method. //////////
+        code = zbicgstabl(mat, b, ell, tol, max_iter, x);
+
+        ////////// Finalization //////////
+        // store approximate solution for MATLAB.
+        for (i = 0; i < size; i++) {
+            x_re[i] = creal(x[i]);
+            x_im[i] = cimag(x[i]);
+        }
+        mxSetPr(*plhs, x_re);
+        mxSetPi(*plhs, x_im);
+
+        free(mat);
+        free(b);
+        free(x);
+    }
 
     if (code < 0) {
         printf("The BiCGSTAB(l) Method did not converged.\n");
@@ -46,9 +102,5 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs,
     else {
         printf("The BiCGSTAB(l) Method converged at iteration %d.\n", code);
     }
-    printf("The true relative residual 2-norm : %e\n",
-           d2norm_true_res_u(mat, b, x));
-
-    free(mat);
 }
 
